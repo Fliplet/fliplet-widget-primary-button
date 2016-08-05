6c1a6bb9d162537e8094f40bfc2c9faaab99b42a
Nicholas Valbusa <squallstar@gmail.com>
2016-08-05 18:02:13 +0200

    new logic for providers

--- js/interface.js
+++ js/interface.js
@@ -1,20 +1,28 @@
 var data = Fliplet.Widget.getData() || {};
 
-Fliplet.Widget.open('com.fliplet.link', {
+var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
   // If provided, the iframe will be appended here,
   // otherwise will be displayed as a full-size iframe overlay
   selector: '#action',
   // Also send the data I have locally, so that
   // the interface gets repopulated with the same stuff
-  data: data.action,
-}).then(function (result) {
-  data.action = result.data;
+  data: data.action
+});
+
+// 1. Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
   $('form').submit();
 });
 
+// 2. Fired when the user submits the form
 $('form').submit(function (event) {
   event.preventDefault();
+  linkActionProvider.forwardSaveRequest();
+});
 
+// 3. Fired when the provider has finished
+linkActionProvider.then(function (result) {
+  data.action = result.data;
   data.label = $('#buttonLabel').val();
 
   Fliplet.Widget.save(data).then(function () {
