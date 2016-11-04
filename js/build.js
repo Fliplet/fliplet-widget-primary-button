9c2740d1dc68167df76b247dc863f2a482579512
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-04 10:54:32 +0000

    refactor click

--- js/build.js
+++ js/build.js
@@ -1,7 +1,7 @@
-$('[data-button-id]').click(function (event) {
+$('[data-primary-button-id]').click(function (event) {
   event.preventDefault();
 
-  var data = Fliplet.Widget.getData($(this).data('button-id'));
+  var data = Fliplet.Widget.getData($(this).attr('data-primary-button-id'));
 
   Fliplet.Navigate.to(data.action);
 });
