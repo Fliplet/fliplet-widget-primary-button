7bd9f6028f767df1d09693cf501e99c274a38f1b
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-04 11:19:17 +0000

    tweak

--- js/build.js
+++ js/build.js
@@ -1,7 +1,7 @@
 $('[data-primary-button-id]').click(function (event) {
   event.preventDefault();
 
-  var data = Fliplet.Widget.getData($(this).attr('data-primary-button-id'));
+  var data = Fliplet.Widget.getData($(this).data('primary-button-id'));
 
   Fliplet.Navigate.to(data.action);
 });
