9d755846c31b193b0d0098280439f80f6ce2134e
Nicholas Valbusa <squallstar@gmail.com>
2019-12-02 11:38:07 +0000

    tweaks

--- js/build.js
+++ js/build.js
@@ -1,6 +1,9 @@
 Fliplet.Widget.instance('primary-button', function (data) {
   $(this).click(function (event) {
     event.preventDefault();
-    Fliplet.Navigate.to(data.action);
+
+    if (data.action) {
+      Fliplet.Navigate.to(data.action);
+    }
   });
 });
